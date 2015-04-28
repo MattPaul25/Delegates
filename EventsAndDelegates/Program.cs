2e9f53d34903e75b086966718093827740e0b013
Matt Farguson <xlsxgeek@gmail.com>
2015-04-28 00:48:00 -0700

    first and probably last commit of delegate sample

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+            var textService = new TextService();
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded; //subscribing to the method
+            videoEncoder.VideoEncoded += textService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+            Console.ReadLine();
+        }
+
+    }
+    public class VideoEventArgs : EventArgs
+    {
+        //the optional event args must inherit from the EventARgs object if deciding to use it
+        public Video vid { get; set; }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs args)
+        {
+            Console.WriteLine("MailService: Sending an email..." + args.vid.Title);
+        }
+    }
+
+    public class TextService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs args)
+        {
+            Console.WriteLine("TextService: Sending Text " + args.vid.Title);
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventsAndDelegates
+{
+   public class VideoEncoder
+    {
+        // 1 - Define a delegate (contract between publisher and subscriber)
+        // 2 - Define an event based on that delegate
+        //3 - Raise the event
+
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args); //1. declaring a delegate - determines the signature of the subscriber
+
+
+        public event VideoEncodedEventHandler VideoEncoded; //2. defining the event 
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video");
+            System.Threading.Thread.Sleep(3000);
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video myVideo)
+        { 
+            //3. raises the event (always uses proteted virtual void)
+            if (VideoEncoded != null) //null would mean that nobody is subscribed to this event
+            {
+                VideoEncoded(this, new VideoEventArgs() { vid = myVideo });
+            }
+        }
+    }
+}
